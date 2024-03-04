1b4f7e4d0baec9bb2d2feed71c4e1dea4a4975d3
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-03-04 14:03:21 -0500

    Add updated NextJS jest testing configs

--- app/components/HomePage.test.tsx
+++ app/components/HomePage.test.tsx
@@ -1,11 +1,18 @@
+import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import HomePage from "./HomePage";
-import "@testing-library/jest-dom";
 
 describe("Home Page", () => {
-    test("Renders Home Page with title 'Home Page'", () => {
+    it("renders a heading", () => {
         render(<HomePage />);
-        const result = screen.getByText("Home Page");
-        expect(result).toBeInTheDocument();
+
+        const heading = screen.getByRole("heading", { level: 1 });
+
+        expect(heading).toBeInTheDocument();
+    });
+
+    it("renders HomePage unchanged", () => {
+        const { container } = render(<HomePage />);
+        expect(container).toMatchSnapshot();
     });
 });
