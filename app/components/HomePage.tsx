40c70cb8f9371754a5a721d9a338ca9c1417e121
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-03-04 13:59:51 -0500

    Add nextjs to jest

--- app/components/HomePage.tsx
+++ app/components/HomePage.tsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
+
 const HomePage = () => {
+    const [test, setTest] = useState(false);
+
+    const clickHandler = () => {
+        setTest((currentVal) => {
+            return !currentVal;
+        });
+    };
+
     return (
         <div>
             <h1>Home Page</h1>
+            <button onClick={clickHandler}></button>
         </div>
     );
 };
